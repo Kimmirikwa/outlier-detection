95e5a7e6e2966f0424ada6ef155c98d4961646cd
Kimmirikwa <kimrodrikwa@gmail.com>
2019-11-27 11:08:15 +0300

    better preview and double checking data

--- main.py
+++ main.py
@@ -12,7 +12,17 @@ print(house_prices.columns.tolist())
 house_prices_copy = house_prices.copy(deep=True)
 
 # preview data
+# we only need 'security_level', 'air_quality_index', 'num_key_amenities' and 'house_type'
+# features in this analysis
+# 'security_level', 'air_quality_index' and 'num_key_amenities' need to have the same scale
+# to be used in scoring zones.
+# 'house_type' need to be numerical to be used in our model 
 print(house_prices_copy.info()) # 10010 rows and 10 columns
+scoring_features = ['security_level', 'air_quality_index', 'num_key_amenities']
+house_type = ['house_type']
+analysis_features = house_type + scoring_features
+print(house_prices_copy[ scoring_features].describe())
+
 
 # Cleaning data: correcting, completing, creating, converting
 
@@ -23,9 +33,8 @@ print(house_prices_copy.info()) # 10010 rows and 10 columns
 # 2. completing
 # we look for any missing values. If we have any missing values, we should attempt to
 # fill tha blank data points with some appropriate values
-# we find that both 'num_bedrooms' and 'num_baths' have 3003 missing values, while the rest of the columns
-# have all the values. Since we are not interested in these 2 columns, we do not need to waste time completing them
-print("The number of columns with missing values: ", house_prices_copy.isnull().sum())
+# our relevant features do not have any missing values
+print(house_prices_copy[analysis_features].isnull().sum())
 
 # 3. creating
 # we are not going to do any feature engineering as we do not need any new features here
@@ -34,11 +43,13 @@ print("The number of columns with missing values: ", house_prices_copy.isnull().
 # we going to convert 'security_level', 'air_quality_index', 'num_key_amenities' to be in scale 0-1 so as
 # to use them to score the 7 zones using their weighted averages (avoid comparing apples to oranges)
 scaler = MinMaxScaler()
-scoring_features = ['security_level', 'air_quality_index', 'num_key_amenities']
-scoring_data = house_prices_copy[scoring_features]
-house_prices_copy[scoring_features] = scaler.fit_transform(scoring_data)
+house_prices_copy[scoring_features] = scaler.fit_transform(house_prices_copy[scoring_features])
 # we now encode the 'house_type' to have values between '0' and '3' as our
 # learning model works with numericals only
 encoder = LabelEncoder()
 house_prices_copy['house_type_code'] = encoder.fit_transform(house_prices_copy['house_type'])
+
+# double checking to ensure our data is OK
+print(house_prices_copy[scoring_features + ['house_type_code']].info())  # all features are numerical
+print(house_prices_copy[scoring_features].describe()) # values range from '0' to '1'
 
